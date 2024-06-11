820a2ed8539673830b6578ce60a743f52bd987e2
shvnsaha <shovan.css2018@gmail.com>
2024-06-12 01:24:56 +0600

    Initialize project setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app'
+import mongoose from 'mongoose'
+import config from './app/config'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+main()
